563bd436cfd17099df7250d2652c46df7a477767
misobae <misobae0731@gmail.com>
2024-03-17 15:44:53 +0900

    Feat: Recoil 세팅

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RecoilRoot } from 'recoil';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from "react-helmet-async";
 
 import App from './App';
+
 import { GlobalStyle } from './styles/GlobalStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <HelmetProvider>
-        <GlobalStyle />
-        <App />
-      </HelmetProvider>
-    </BrowserRouter>
+    <RecoilRoot>
+      <BrowserRouter>
+        <HelmetProvider>
+          <GlobalStyle />
+          <App />
+        </HelmetProvider>
+      </BrowserRouter>
+    </RecoilRoot>
   </React.StrictMode>
 );
---
+++ src/state/atoms.js
@@ -0,0 +1,6 @@
+import { atom } from "recoil";
+
+export const lightState = atom({
+  key: 'lightState',
+  default: 0,
+})
