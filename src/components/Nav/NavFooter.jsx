05e56377ec28f928e2b95658ac2680032970dafa
misobae <misobae0731@gmail.com>
2024-03-18 15:56:09 +0900

    Design: 스타일 수정

--- src/components/Nav/NavFooter.jsx
+++ src/components/Nav/NavFooter.jsx
@@ -1,13 +1,28 @@
 import Nav from "./Nav";
 
 import { Wrapper } from "./NavFooter.style";
+const Vars = {
+  init: {
+    opacity: 0,
+    transition: { duration: 0, delay: 0 },
+  },
+  ani: {
+    opacity: 1,
+    transition: { duration: 0.6, delay: 0.7 }
+  },
+  exit: {
+    opacity: 0,
+    transition: { duration: 0, delay: 0 },
+  }
+}
 
 function NavFooter() {
   return (
     <Wrapper
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 1 }}
+      variants={Vars}
+      initial="init"
+      animate="ani"
+      exit="exit"
     >
       <Nav />
     </Wrapper>
--- src/components/Nav/NavHeader.jsx
+++ src/components/Nav/NavHeader.jsx
@@ -1,10 +1,29 @@
 import Nav from "./Nav";
 
 import { Wrapper } from "./NavHeader.style";
+const Vars = {
+  init: {
+    opacity: 0,
+    transition: { duration: 0 },
+  },
+  ani: {
+    opacity: 1,
+    transition: { duration: 0.6 }
+  },
+  exit: {
+    opacity: 0,
+    transition: { duration: 0 },
+  }
+}
 
 function NavHeader() {
   return (
-    <Wrapper>
+    <Wrapper
+      variants={Vars}
+      initial="init"
+      animate="ani"
+      exit="exit"
+    >
       <Nav />
     </Wrapper>
   )
