3b0a2eeb2152d42276a7a9c106a0f2df02017269
misobae <misobae0731@gmail.com>
2024-03-18 16:01:18 +0900

    Feat: Home 페이지 내용 수정

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,25 +1,8 @@
 import React from 'react';
 
-import NavFooter from "../components/Nav/NavFooter";
-
 function Home() {
   return (
-    <>
-      <NavFooter
-      initial={{
-        opacity: 0,
-        transform: "translate3d(0, 20%, 0)",
-      }}
-        animate={{
-          opacity: 1,
-          transform: "translate3d(0, 0, 0)",
-        }}
-        exit={{
-          opacity: 0,
-          transform: "translate3d(0, 20%, 0)",
-        }}
-        transition={{ duration: 0.5 }} />
-    </>
+    <></>
   )
 }
 
