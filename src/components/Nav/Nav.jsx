0bad88abef792c8d3bb7db76081adacadc6a30e6
misobae <misobae0731@gmail.com>
2024-03-21 13:47:30 +0900

    Feat: 현재 경로에 따라 Nav에  Light 컴포넌트 조건부 렌더링 추가

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,14 +1,45 @@
 import { Link, useLocation } from "react-router-dom";
+import { Light } from "./NavHeader.style";
 
 function Nav() {
   const location = useLocation();
 
   return (
     <>
-      {location.pathname === "/" ? "" : <Link to="/">HOME</Link>}
-      <Link to="/about">ABOUT</Link>
-      <Link to="/projects">PROJECTS</Link>
-      <Link to="/experience">EXPERIENCE</Link>
+      {location.pathname === "/" ? null : (
+        <Link to="/">
+          {location.pathname === "/" ? (
+            <>
+              <span className="active">HOME</span>
+              <Light layoutId="light" />
+            </>
+          ): <span>HOME</span>}
+        </Link>
+      )}
+      <Link to="/about">
+        {location.pathname === "/about"  ? (
+          <>
+            <span className="active">ABOUT</span>
+            <Light layoutId="light" />
+          </>
+        ): <span>ABOUT</span>}
+      </Link>
+      <Link to="/projects">
+        {location.pathname === "/projects" || location.pathname.startsWith("/projects/") ? (
+          <>
+            <span className="active">PROJECTS</span>
+            <Light layoutId="light" />
+          </>
+        ): <span>PROJECTS</span>}
+      </Link>
+      <Link to="/experience">
+        {location.pathname === "/experience"  ? (
+          <>
+            <span className="active">EXPERIENCE</span>
+            <Light layoutId="light" />
+          </>
+        ): <span>EXPERIENCE</span>}
+      </Link>
     </>
   )
 }
--- src/components/Nav/NavHeader.style.js
+++ src/components/Nav/NavHeader.style.js
@@ -11,7 +11,9 @@ export const Wrapper = styled(motion.nav)`
   font-size: 15px;
   
   a {
+    position: relative;
     color: var(--color-white);
+    transition: 0.3s;
 
     &:not(:last-child)::after {
       content: "|";
@@ -19,5 +21,25 @@ export const Wrapper = styled(motion.nav)`
       margin-left: 1.5em;
       font-weight: 300;
     }
+
+    span {
+      opacity: 0.6;
+    }
+
+    .active {
+      opacity: 1;
+    }
   }
+`;
+
+export const Light = styled(motion.span)`
+  position: absolute;
+  z-index: -1;
+  top: 50%;
+  left: 42%;
+  width: 0;
+  height: 0;
+  border-radius: 50%;
+  box-shadow: 0 0 40px 16px rgba(255, 255, 255, 70%);
+  transform: translate(-50%, -50%);
 `;
